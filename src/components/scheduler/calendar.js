ee3f4f4cb15ebbe67e069b0c241c65c96315b46a
Namisus <berniceseah2000@gmail.com>
2020-11-04 17:31:11 +0800

    Retrieve calendar values and moved styling to Calendar.css

--- src/components/scheduler/calendar.js
+++ src/components/scheduler/calendar.js
@@ -1,20 +1,43 @@
 import React, { Component } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import '../../styles/Calendar.css';
  
 class calendar extends Component {
   state = {
     date: new Date(),
+  };
+
+  parseISOString(s) {
+    var b = s.split(" ");
+    return new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
   }
  
   onChange = date => this.setState({ date })
   render() {
     return (
-      <div style={{display: 'flex', justifyContent: 'center'}}>
-        <Calendar
-          onChange={this.onChange}
-          value={this.state.date}
-        />
+        // add styling here 
+      <div className={"calendar_whole"}>
+          <div className={"calendar_title"}> BOOK YOUR DATE! </div>
+
+          <div className={"calendar"}>
+            <Calendar className={"calendar_design"}
+            onChange={this.onChange}
+            value={this.state.date}
+            />
+          </div>
+
+        {/* to extract ISO datetime from calendar */}
+        <div>{this.state.date.toString()}</div>
+
+        {/* why run twice de o.o  */}
+        <div>{console.log(this.state.date.toString())}</div>
+
+        {/* tried calling the parse function but got some error :( ) */}
+        {/* {this.parseISOString(this.state.date.toString())}    */}
+        
+        <div>hello</div>
+
       </div>
     );
   }
